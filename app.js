c4633a847a8657c36dc5988b11fd4933bb363db5
thops1025 <clumen600@gmail.com>
2021-08-20 20:17:06 +0800

    this is it

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-require('dotenv').config({ path: '.env' });
+require('dotenv').config();
 
 const todo = require('./routes/TodoRoutes')
 
