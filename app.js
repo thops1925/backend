17537bd489dd14eca9bcfef33f25129bdfa31604
thops1025 <clumen600@gmail.com>
2021-08-20 19:33:48 +0800

    revertzzz

--- app.js
+++ app.js
@@ -9,22 +9,24 @@ const Port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+const mongoURI = process.env.URIT;
 mongoose
     .connect(
-        process.env.URIT,
+        mongoURI,
         { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => {
         console.log('MongoDB Connected');
     })
     .catch(err => console.log(err));
-mongoose.connection
-    .once('open', function () {
-        console.log('MongoDB running');
-    })
-    .on('error', function (err) {
-        console.log(err);
-    });
+
+mongoose.connection.openUri(mongoURI, function (err) {
+    if (err) {
+        console.log("Failed to connect to database: " + err);
+    } else {
+        console.log("Successfully connected to database. ");
+    }
+});
 
 
 app.use('/thops', todo)
