47be5d888826d8924c8d8313f2b767f44e4fb0c6
thops1025 <clumen600@gmail.com>
2021-08-20 19:42:45 +0800

    revertzzzz

--- app.js
+++ app.js
@@ -5,29 +5,27 @@ const todo = require('./routes/TodoRoutes')
 require('dotenv').config()
 
 const app = express()
-const Port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const mongoURI = process.env.URIT;
 mongoose
     .connect(
-        mongoURI,
+        process.env.URIT,
         { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => {
         console.log('MongoDB Connected');
     })
     .catch(err => console.log(err));
+mongoose.connection
+    .once('open', function () {
+        console.log('MongoDB running');
+    })
+    .on('error', function (err) {
+        console.log(err);
+    });
 
-mongoose.connection.openUri(mongoURI, function (err) {
-    if (err) {
-        console.log("Failed to connect to database: " + err);
-    } else {
-        console.log("Successfully connected to database. ");
-    }
-});
 
 
 app.use('/thops', todo)
-app.listen(Port, () => console.log('listening on port'))
+app.listen(process.env.PORT || 5000, () => console.log('listening on port'))
