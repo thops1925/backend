79c72edd3f64d519d7d8a6d7bef0641d0f9e8fee
thops1025 <clumen600@gmail.com>
2021-08-20 18:56:50 +0800

    revertz

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ const Port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const db = process.env.URIT;
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoURI = process.env.URIT;
+mongoose
+    .connect(
+        mongoURI,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => {
+        console.log('MongoDB Connected');
+    })
+    .catch(err => console.log(err));
 mongoose.connection
     .once('open', function () {
         console.log('MongoDB running');
